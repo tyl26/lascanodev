4a2f3e00a2aed8cfb44284221278a7a47b4fbcb4
Yvon Lascano <lascanoyvon@gmail.com>
2023-01-05 09:11:20 +0100

    small fixes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,35 +3,25 @@ import Hamters from "../Img/Hamsters.png"
 import hangman from "../Img/hangman.png"
 import weddingCamera from "../Img/wedding-camera.png"
 import beers from "../Img/beers.png"
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
 function Projects() {
-// const[showTitle, setShowTitle]= useState(false)
 
-// const showProjectTitle = () =>{
-//   setShowTitle(true)
-// }
-// const hideProjectTitle = () =>{
-//   setShowTitle(false)
-// }
 
   return (
     <section id='project' className='project-container container'>
       <h2>Projects.</h2>
 
-{/* Gör en json fil för pjoect bilderna */}
+
 <div className='project-wrapper'>
-      <a href='' className='project'>
+      <a href='https://github.com/tyl26/Hamsterwars' className='project'>
       <img src={Hamters} alt="hamster-ptoject" />
-      {/* <p>{showTitle ? "hamsters": ""}</p> */}
       </a>
-      <a href='' className='project'>
+      <a href='https://github.com/tyl26/Hangman-' className='project'>
       <img src={hangman} alt="hangman-ptoject" />
       </a>
-      <a href='' className='project'>
+      <a href='https://github.com/tyl26/Wedding-Camera-app' className='project'>
       <img src={weddingCamera} alt="weddingcamera-ptoject" />
       </a>
-      <a href='' className='project'>
+      <a href='https://github.com/tyl26/Beers' className='project'>
       <img src={beers} alt="beers-ptoject" />
       </a>
 </div>     
